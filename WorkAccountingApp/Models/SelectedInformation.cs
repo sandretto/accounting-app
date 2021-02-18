d35633966492640260db5814b74c701301bc9389
sandretto <alepet.petr1638@yandex.ru>
2021-02-18 15:12:55 +0300

    Add selected information model

---
+++ WorkAccountingApp/Models/SelectedInformation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkAccountingApp.Models
+{
+    public class SelectedInformation
+    {
+        public string Id { get; set; }
+        public string City { get; set; }
+        public string Department { get; set; }
+        public string Employee { get; set; }
+        public string Team { get; set; }
+        public string Shift { get; set; }
+    }
+}
