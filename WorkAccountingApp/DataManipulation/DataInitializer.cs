efce33c02238a9dba37872635ab4b38620bc9f5d
sandretto <alepet.petr1638@yandex.ru>
2021-02-18 12:42:39 +0300

    Add data manipulation classes

---
+++ WorkAccountingApp/DataManipulation/DataInitializer.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using WorkAccountingApp.Models;
+
+namespace WorkAccountingApp.DataManipulation
+{
+    public class DataInitializer
+    {
+        private const string citiesPath = "Cities.json";
+        private const string departmentsPath = "Departments.json";
+        private const string employeesPath = "Employees.json";
+
+        public static async Task<List<City>> GetCities(EFDbContext dbContext)
+        {
+            var json = File.ReadAllText(@"Seed" + Path.DirectorySeparatorChar + citiesPath);
+            var list = JsonConvert.DeserializeObject<List<City>>(json);
+            await dbContext.AddRangeAsync(list);
+            await dbContext.SaveChangesAsync();
+            return list;
+        }
+
+        public static async Task<List<Department>> GetDepartments(EFDbContext dbContext)
+        {
+            var json = File.ReadAllText(@"Seed" + Path.DirectorySeparatorChar + departmentsPath);
+            var list = JsonConvert.DeserializeObject<List<Department>>(json);
+            await dbContext.AddRangeAsync(list);
+            await dbContext.SaveChangesAsync();
+            return list;
+        }
+
+        public static async Task<List<Employee>> GetEmployees(EFDbContext dbContext)
+        {
+            var json = File.ReadAllText(@"Seed" + Path.DirectorySeparatorChar + employeesPath);
+            var list = JsonConvert.DeserializeObject<List<Employee>>(json);
+            await dbContext.AddRangeAsync(list);
+            await dbContext.SaveChangesAsync();
+            return list;
+        }
+    }
+}
---
+++ WorkAccountingApp/DataManipulation/EFDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using WorkAccountingApp.Models;
+
+namespace WorkAccountingApp.DataManipulation
+{
+    public class EFDbContext : DbContext
+    {
+        public DbSet<City> Cities { get; set; } 
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+
+        public EFDbContext()
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(
+            DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionStringBuilder = new SqliteConnectionStringBuilder { DataSource = "accounting.db"};
+            var connectionString = connectionStringBuilder.ToString();
+            var connection = new SqliteConnection(connectionString);
+            optionsBuilder.UseSqlite(connection);
+            optionsBuilder.UseLazyLoadingProxies();
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
