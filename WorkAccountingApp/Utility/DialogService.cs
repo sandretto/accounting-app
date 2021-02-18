967e3c9f6de6732b5995a611cbbb86bc5175d811
sandretto <alepet.petr1638@yandex.ru>
2021-02-18 15:13:12 +0300

    Add utility classes

---
+++ WorkAccountingApp/Utility/DialogService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+
+namespace WorkAccountingApp.Utility
+{
+    public interface IDialogService
+    {
+        string FilePath { get; set; } 
+        void ShowMessage(string message);
+        bool SaveFileDialog();
+    }
+
+    public class DialogService : IDialogService
+    {
+        public string FilePath { get; set; }
+
+        public bool SaveFileDialog()
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                FilePath = saveFileDialog.FileName;
+                return true;
+            }
+            return false;
+        }
+
+        public void ShowMessage(string message)
+        {
+            MessageBox.Show(message);
+        }
+    }
+}
---
+++ WorkAccountingApp/Utility/FileService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Text;
+
+namespace WorkAccountingApp.Utility
+{
+    public interface IFileService<T>
+    {
+        public void Save(string filename, T listToSave); 
+    }
+
+    public class JsonFileService<T> : IFileService<T>
+    {
+        public void Save(string filename, T listToSave)
+        {
+            DataContractJsonSerializer jsonFormatter =
+                new DataContractJsonSerializer(typeof(T));
+            using FileStream fs = new FileStream(filename, FileMode.Create);
+            jsonFormatter.WriteObject(fs, listToSave);
+        }
+    }
+ }
